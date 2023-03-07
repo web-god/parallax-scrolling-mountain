aa8e9b7a0e574c5b8c68129f6004368bcff79e14
RashidIqbal-mri <rashidiqbal23705@gmail.com>
2023-03-07 19:27:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import {Parallax,ParallaxLayer} from "@react-spring/parallax";
 import './App.css';
+import Content from "./Content";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+        <Parallax pages={2} style={{ top: '0', left: '0' }} class="animation">
+        <ParallaxLayer offset={0} speed={0.25}>
+          <div class="animation_layer parallax" id="artback"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.3}>
+          <div class="animation_layer parallax" id="mountain"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={-0.1}>
+          <div class="animation_layer parallax" id="logoland"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.3}>
+          <div class="animation_layer parallax" id="jungle1"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.35}>
+          <div class="animation_layer parallax" id="jungle2"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.5}>
+          <div class="animation_layer parallax" id="jungle3"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.45}>
+          <div class="animation_layer parallax" id="jungle4"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.40}>
+          <div class="animation_layer parallax" id="manonmountain"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} speed={0.35}>
+          <div class="animation_layer parallax" id="jungle5"></div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.25}>
+          <Content />
+        </ParallaxLayer>
+      </Parallax>
+   </>
   );
 }
 
---
+++ src/Content.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div id="content">
+            <div id="content-container">
+                <h1 id="content-title">The best way to predict the future is to invent it.</h1>
+                <p id="content-paragraph">
+                Design is not just what it looks like and feels like. Design is how it works.<br/><br/>
+                Front-end development is about creating the perfect marriage between form and function.<br/><br/>
+                A good front-end developer is always learning and always experimenting with new techniques and technologies.<br/><br/>
+                In the end, the front-end code is what the user interacts with, so it's important to make it as seamless and delightful as possible.
+                </p>
+            </div>
+            <footer id='content-footer'> &#169;&#65039;<a id="content-devsense" href="https://rashid-parallax.netlify.app/">Rashid Iqbal</a></footer>
+        </div>
+  )
+}
+
+export default Content
